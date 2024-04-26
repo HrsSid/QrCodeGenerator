02a4fbbbc7153ae935e853dc9c20349bd6c953dd
G6H4 <proplopaido@gmail.com>
2024-04-26 23:25:27 +0200

    major: V1.0 - user is able to customize only content, adding version + syntax instructions + signature

--- Source Code/qr.py
+++ Source Code/qr.py
@@ -1,4 +1,6 @@
 # Libraries
+import sys
+
 from qrcode import ERROR_CORRECT_L
 from qrcode.main import QRCode
 
@@ -11,8 +13,8 @@ class QrCodeGenerator:
         qrFileName: str = "qrcode.png",
         qrAccentColor: str = "#000000",
         qrBackgroundColor: str = "#ffffff",
-    ):
-        """This class is used to generate QR code image in the `.png` format
+    ) -> None:
+        """This class is used to initiate the generation of the QR code in the `.png` format
 
         Args:
             qrContent (str, optional): The content of the QR code. Defaults to `This is the content of this qr code`.
@@ -29,7 +31,53 @@ class QrCodeGenerator:
         # Creating the QR Code object
         self.qrCode = QRCode(error_correction=ERROR_CORRECT_L)
 
+    def checkFileName(self, fileName: str) -> str:
+        """Check if the file name that was given meets the criteria of the naming process
 
-# Main program
+        Args:
+            fileName (str): The file name of the QR code generated
+
+        Returns:
+            str: Returns a file name based on the simple rules of the naming process. Ideal file name is `*.png`
+        """
+
+        # example -> fileName="" (<- Null) -> fileName="qrcode"
+        if fileName == "":
+            fileName = "qrcode"
+        # example -> fileName=".png" (<- No name but correct extension) -> fileName="qrcode"
+        if fileName.split(".")[0] == "":
+            fileName = "qrcode"
+        # example -> fileName="qrcode.jpg" -> fileName="qrcode.jpg.png" (The last extension is the one that matters)
+        if fileName.split(".")[1] != "png":
+            fileName += ".png"
+        return fileName  # returns the corrected file name (if correction were made, otherwise returns the original name given)
+
+    def generate(self) -> None:
+        """This function generates the QR code in the `.png` format"""
+        self.qrCode.add_data(self.content)  # adding the content of the QR code
+        self.qrCode.make(fit=True)  # not really sure what this does xD
+        self.qrImage = self.qrCode.make_image(
+            fill_color=self.accentColor, back_color=self.backgroundColor
+        )  # editing the QR code accent color and background color
+        self.fileName = self.checkFileName(self.fileName)  # correcting the file name
+        self.qrImage.save(self.fileName)  # saving the QR code
+
+
+# Putting everything together
 if __name__ == "__main__":
-    pass
+    arguments = sys.argv  # command syntax: python qr.py {content}
+    arguments = arguments[1:]  # removing the file path from the arguments (because it isn't an argument xD)
+    # checking if the arguments are correct
+    if len(arguments) == 0 or len(arguments) > 1:
+        raise SyntaxError(
+            "missing argument {qrContent}"
+        )  # raising an error if the arguments are not correct
+    # generating the QR code
+    qr = QrCodeGenerator(
+        qrContent=arguments[0]
+    )  # creating the qr class and passing the content argument
+    qr.generate()  # generating and saving the QR code
+
+# VERSION: 1.0
+# Syntax: python qr.py {content}
+# Made by Harry Sidiropoulos
